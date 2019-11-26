76774d3b8844561219871de8e405a0b1058b5d71
Simon Sorensen <teamangryghast@gmail.com>
2019-11-26 10:28:21 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sussex',
-    version='0.4',
+    version='0.5.1',
     license='MIT',
     author = 'Simon Sorensen',
     author_email='hello@simse.io',
