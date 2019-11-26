128122c77c45d04b3024fcd84d6e008a4f7e07bc
hello@simse.io <simon@localhost.localdomain>
2019-11-26 12:54:30 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sussex',
-    version='0.5.1',
+    version='0.5.2',
     license='MIT',
     author = 'Simon Sorensen',
     author_email='hello@simse.io',
